3af0b6961373ca483f38ae6741121ac7d6155237
Matthew Kitt <mk.kitt@gmail.com>
2010-04-29 19:39:48 -0600

    Minor refactoring around the action method.
    Still a ways to go and not quite usable.

--- test/helpers/nested/helper.js
+++ test/helpers/nested/helper.js
@@ -1,1 +1,1 @@
-// Do not edit. File is overwritten for unit tests! => Thu Apr 29 2010 17:02:02 GMT-0600 (MDT)
+// Do not edit. File is overwritten for unit tests! => Thu Apr 29 2010 19:37:47 GMT-0600 (MDT)
--- test/helpers/nested/uber-nested/helper.js
+++ test/helpers/nested/uber-nested/helper.js
@@ -1,1 +1,1 @@
-// Do not edit. File is overwritten for unit tests! => Thu Apr 29 2010 17:02:12 GMT-0600 (MDT)
+// Do not edit. File is overwritten for unit tests! => Thu Apr 29 2010 19:37:57 GMT-0600 (MDT)
