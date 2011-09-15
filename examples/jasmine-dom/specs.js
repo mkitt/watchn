2df66b6c6469cbd580ef11cb1713df179b45475a
Matthew Kitt <mk.kitt@gmail.com>
2011-09-15 01:28:13 -0600

    Adding a jasmine-dom reporter

---
+++ examples/jasmine-dom/specs.js
@@ -0,0 +1,14 @@
+
+(function() {
+
+  describe('#namespace', function() {
+    it('should just fail', function() {
+      expect(true).toEqual(true)
+    })
+    it('should just report something', function() {
+      expect(true).toEqual(true)
+    })
+  })
+
+}());
+
---
+++ test/reporters/jasmine_dom_reporter.test.js
@@ -0,0 +1,55 @@
+
+var assert = require('assert')
+var JasmineDomReporter = require('reporters/jasmine_dom_reporter')
+var reporter
+
+function before() {
+  reporter = new JasmineDomReporter()
+}
+
+module.exports = {
+  'test #constructor': function() {
+    before()
+    assert.eql(reporter.name, 'Jasmine Dom')
+  },
+
+  'test #inherited the trim function': function() {
+    before()
+    assert.isDefined(reporter.trim(''))
+  },
+
+  'test #passed with a failure': function() {
+    before()
+    var stdout = 'Failed:\n\t - In index.html >> #namespace >> should just fail :: Expected false to equal true.'
+    var report = reporter.passed(null, stdout, 'stderr')
+    assert.eql(report.name, reporter.failed_message)
+    assert.eql(report.msg, reporter.name + ': ' + stdout)
+    assert.eql(report.gmsg, '1 spec failed')
+  },
+
+  'test #passed for realz': function() {
+    before()
+    var stdout = 'Passed'
+    var report = reporter.passed(null, stdout, 'stderr')
+    assert.eql(report.name, reporter.passed_message)
+    assert.eql(report.msg, reporter.name + ': ' + stdout)
+    assert.eql(report.gmsg, 'Passed')
+  },
+
+  'test #failed': function() {
+    before()
+    var stdout = 'Jasmine Dom: Failed:\n\t - In index.html >> #namespace >> should just fail :: Expected false to equal true.'
+    var report = reporter.failed({msg: 'failed'}, stdout, 'stderr')
+    assert.eql(report.name, reporter.failed_message)
+    assert.eql(report.msg, stdout)
+    assert.eql(report.gmsg, '1 spec failed')
+  },
+
+  'test #results': function() {
+    before()
+    var stdout = 'Jasmine Dom: Failed:\n\t - In index.html >> #namespace >> should just fail :: Expected false to equal true.'
+    assert.eql(reporter.results(stdout), '1 spec failed')
+  }
+
+}
+
